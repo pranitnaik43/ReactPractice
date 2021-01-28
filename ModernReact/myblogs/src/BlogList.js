a96318973f3a8f9dc66573d65e86f33eed28346d
pranitnaik43 <pranitnaik43@gmail.com>
2021-01-28 15:36:23 +0530

    react modern part-13 using functions as props

--- ModernReact/myblogs/src/BlogList.js
+++ ModernReact/myblogs/src/BlogList.js
@@ -1,6 +1,7 @@
 const BlogList = (props) => {
     const blogs = props.blogs
     const title = props.title
+    const HandleDelete = props.HandleDelete
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
@@ -8,6 +9,7 @@ const BlogList = (props) => {
                 <div className="blog-preview">
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
+                    <button onClick={() => {HandleDelete(blog.id)}}>Delete</button>
                 </div>
             ))}
         </div>
